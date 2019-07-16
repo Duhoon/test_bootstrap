7a72bbfa87d590b9cfbb6cf963f782b1205876fb
Duhoon <eclip6@naver.com>
2019-07-16 15:09:46 +0900

    grid add

--- app.js
+++ app.js
@@ -45,6 +45,16 @@ router.get('/3kutilities', function (req, res) {
     //__dirname : It will resolve to your project folder.
 });
 
+router.get('/3kutilities', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/4jgrid.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+router.get('/3kutilities', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/4kgrid.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
 
 
 app.use('/', router);
